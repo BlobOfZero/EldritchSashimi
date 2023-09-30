f5b6bdfac32568e7afd77ddae1e2a35b2788a778
BlobOfZero <erinnbowser@yahoo.com>
2023-09-30 13:22:46 -0400

    Movement/Camera

---
+++ EldritchSashimi/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Vector2 move;
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        move = context.ReadValue<Vector2>();
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    public void MovePlayer()
+    {
+        Vector3 movement = new Vector3(move.x, 0f, move.y);
+
+        if(movement != Vector3.zero) 
+        { 
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(movement), 0.15f);
+        }
+        
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+    }
+}
