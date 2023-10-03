ba9fc1331265ffefe59e6960902e31602a088df9
treemanrocket <56743498+treemanrocket@users.noreply.github.com>
2023-10-02 21:45:53 -0400

    updated enemy  stuff

---
+++ EldritchSashimi/Assets/Scripts/EnemyScripts/EnemyRanged.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+public class EnemyRanged : MonoBehaviour
+{
+    public NavMeshAgent agent;
+
+    public Transform player;
+
+    public float speed;
+
+    [SerializeField] private LayerMask WhatIsPlayer;
+
+    [SerializeField] private bool IsChasing;
+
+    [SerializeField] private bool IsAttacking;
+
+    [SerializeField] private bool PlayerInAttackRange;
+
+    [SerializeField] private float AttackRange;
+
+    void awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+   
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerInAttackRange = Physics.CheckSphere(transform.position, AttackRange, WhatIsPlayer);
+
+        if (!PlayerInAttackRange)
+        {            
+            IsAttacking = false;
+            IsChasing = true;
+            Chasing();
+        }
+
+        if (PlayerInAttackRange)
+        {
+            IsChasing = false;
+            IsAttacking = true;
+        }           
+        
+    }
+
+
+    void Chasing()
+    {
+        if (IsChasing) 
+        {
+            agent.SetDestination(player.position);
+        }        
+        transform.LookAt(player);      
+    }
+
+}
