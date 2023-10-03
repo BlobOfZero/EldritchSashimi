02bb04ab0e5be96b74f3f4ee85f88e99decbf1e4
treemanrocket <56743498+treemanrocket@users.noreply.github.com>
2023-10-03 14:23:27 -0400

    little variable
    variable to be used later

--- EldritchSashimi/Assets/Scripts/EnemyScripts/EnemyRanged.cs
+++ EldritchSashimi/Assets/Scripts/EnemyScripts/EnemyRanged.cs
@@ -20,6 +20,8 @@ public class EnemyRanged : MonoBehaviour
 
     [SerializeField] private float AttackRange;
 
+    [SerializeField] private float RunAwayDistance;
+
     [SerializeField] private int  health = 3;
 
     void awake()
