92a01ae6e5dccf019c246ed872e692d1b7c5d10f
treemanrocket <56743498+treemanrocket@users.noreply.github.com>
2023-10-06 00:06:57 -0400

    update
    here's the update

--- EldritchSashimi/Assets/Scripts/PlayerScript/PlayerController.cs
+++ EldritchSashimi/Assets/Scripts/PlayerScript/PlayerController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : MonoBehaviour, IDamageablePlayer
 {
     public float speed;
     private Vector2 move;
